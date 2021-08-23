ecf4bcec3516112c84d34d0c0de168f9c268d0f0
Yudota <yuri.fernandes.961@gmail.com>
2021-08-23 01:05:38 -0300

    react components task 01
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/DishdetailComponent/index.js
    	deleted:    src/components/MenuComponent.js
    	new file:   src/components/MenuComponent/index.js
    	modified:   src/index.js
    	modified:   src/shared/dishes.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponent";
+import MenuComponent from "./components/MenuComponent/index";
 import "./App.css";
 import { DISHES } from "./shared/dishes";
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       dishes: DISHES,
     };
@@ -22,7 +21,7 @@ class App extends Component {
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
           </Navbar>
-          <Menu dishes={this.state.dishes} />
+          <MenuComponent dishes={this.state.dishes} />
         </div>
       </>
     );
